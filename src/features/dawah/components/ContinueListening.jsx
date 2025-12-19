9323d94f320cbda3a3cce5ecf4499e66ea25ba7a
Abdulsalam Akinyoola <abdulsalamakinyoola@gmail.com>
2025-12-19 22:22:59 +0100

    style(home): make continue listening section responsive

---
+++ src/features/dawah/components/ContinueListening.jsx
@@ -0,0 +1,42 @@
+import eventOneImg from "../../../assets/eventOne.png";
+import eventTwoImg from "../../../assets/eventtwo.png";
+import eventThreeImg from "../../../assets/eventThree.png";
+import eventFourImg from "../../../assets/eventFour.png";
+
+const events = [
+  eventOneImg,
+  eventTwoImg,
+  eventThreeImg,
+  eventFourImg,
+];
+
+export default function ContinueListening() {
+  return (
+    <section className="py-10 px-5 bg-white rounded-2xl dark:bg-black">
+      <div
+        className="
+          grid 
+          grid-cols-1 
+          sm:grid-cols-2 
+          lg:grid-cols-4 
+          gap-4 
+          place-items-center
+        "
+      >
+        {events.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={`Continue listening ${index + 1}`}
+            className="
+              w-full 
+              max-w-[300px] 
+              h-auto 
+              object-contain
+            "
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
